68ce0a3f94890f1dec10cebadc2415e4b80052f0
pray3m <prem.gtm9@gmail.com>
2023-10-10 13:01:43 +0545

    Initial commit - Setup vite react - Add Navbar and Products components - Add Home and Cart pages

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
