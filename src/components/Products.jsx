531ee2ebdbf4af05131ec28fa82db5cd5c1160c0
pray3m <prem.gtm9@gmail.com>
2023-10-10 14:08:14 +0545

    feat : Add Products in Home Page - Fetch data from fakestoreapi - Display data in Home Page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+  const [products, setProducts] = useState([]);
 
-export default Products
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch("https://fakestoreapi.com/products");
+      const data = await res.json();
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <div className="productsWrapper">
+      {products.map((product) => (
+        <div className="card" key={product.id}>
+          <img src={product.image} alt="" />
+          <h4>{product.title}</h4>
+          <h5>$ {product.price}</h5>
+          <button onClick={() => handleAdd(product)} className="btn">
+            Add to cart
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Products;
